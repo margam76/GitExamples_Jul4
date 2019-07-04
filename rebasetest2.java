560747944763dd4e13999984a60c7ad258fb2a61
Bhavani <bhavv@gmail.com>
2019-07-05 04:18:35 +0530

    Commit CC4 after featurerebase

--- rebasetest2.java
+++ rebasetest2.java
@@ -7,5 +7,8 @@ System.Out.Println("Commit 2");
 System.Out.Println(Commit c3 after creating the branch");
 System.Out.Println("Commit feature f1");
 System.Out.Println("Commit feature f2");
+System.Out.Println("Commit feature f3 after 1st merge");
+System.Out.Println("Commit f4 after 1st merge");
+System.Out.Println("Commit CC4 master branch");
 }
 }
