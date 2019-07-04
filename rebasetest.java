847be7180fdccd7a9abf441cf2a91a76d1771f3f
Bhavani <bhavv@gmail.com>
2019-07-05 03:34:52 +0530

    commit f2 on feature

--- rebasetest.java
+++ rebasetest.java
@@ -5,5 +5,6 @@ public static void main(String[] args)
 System.out.println("Testing rebase/merge in master");
 System.out.println("commit2 on master");
 System.out.println("commitf1 in feature");
+System.out.println("commitf2 in feature");
 }
 }
