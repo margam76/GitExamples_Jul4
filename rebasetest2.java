403e9fdf52f4b786c0b34d8b68321d1ba47fe1da
Bhavani <bhavv@gmail.com>
2019-07-05 03:56:00 +0530

    commit f2 feature2

--- rebasetest2.java
+++ rebasetest2.java
@@ -4,6 +4,7 @@ public static void main(String[] args)
 {
 System.Out.Println("Commit1");
 System.Out.Println("Commit 2");
-System.Out.Println("Commit feature f1")
+System.Out.Println("Commit feature f1");
+System.Out.Println("Commit feature f2");
 }
 }
